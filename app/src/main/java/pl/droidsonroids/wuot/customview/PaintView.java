65f55ed5862025a6fcb8296666f21b3b32b2649a
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2016-12-13 02:56:35 +0100

    attributes added

--- app/src/main/java/pl/droidsonroids/wuot/customview/PaintView.java
+++ app/src/main/java/pl/droidsonroids/wuot/customview/PaintView.java
@@ -1,8 +1,10 @@
 package pl.droidsonroids.wuot.customview;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PointF;
@@ -15,7 +17,7 @@ import android.view.View;
 
 import java.util.LinkedList;
 
-public class DrawingView extends View {
+public class PaintView extends View {
 
 	LinkedList<PointF> line = new LinkedList<>();
 
@@ -23,27 +25,35 @@ public class DrawingView extends View {
 	private final Path path = new Path();
 	Bitmap cacheBitmap;
 
-	public DrawingView(final Context context) {
+	public PaintView(final Context context) {
 		this(context, null);
 	}
 
-	public DrawingView(final Context context, final AttributeSet attrs) {
+	public PaintView(final Context context, final AttributeSet attrs) {
 		this(context, attrs, 0);
 	}
 
-	public DrawingView(final Context context, final AttributeSet attrs, final int defStyleAttr) {
+	public PaintView(final Context context, final AttributeSet attrs, final int defStyleAttr) {
 		this(context, attrs, defStyleAttr, 0);
 	}
 
-	public DrawingView(final Context context, final AttributeSet attrs, final int defStyleAttr, final int defStyleRes) {
+	public PaintView(final Context context, final AttributeSet attrs, final int defStyleAttr, final int defStyleRes) {
 		super(context, attrs, defStyleAttr, defStyleRes);
 
+		final TypedArray attributes = context.obtainStyledAttributes(attrs, R.styleable.PaintView, defStyleAttr, defStyleRes);
+
+		final int color = attributes.getColor(R.styleable.PaintView_paintColor, Color.BLACK);
+		paint.setColor(color);
+
 		final DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
-		final float widthPixels = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8.0f, displayMetrics);
+		final float defaultWidthPixels = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8.0f, displayMetrics);
+		final float strokeWidth = attributes.getDimension(R.styleable.PaintView_paintStrokeWidth, defaultWidthPixels);
+		paint.setStrokeWidth(strokeWidth);
+
+		attributes.recycle();
 
 		paint.setStrokeCap(Paint.Cap.ROUND);
 		paint.setStrokeJoin(Paint.Join.ROUND);
-		paint.setStrokeWidth(widthPixels);
 		paint.setStyle(Paint.Style.STROKE);
 	}
 
